267b2eabe66d5e0cc748b1aea689c8608a28fd82
Joel Hans <joel.g.hans@gmail.com>
2021-04-28 10:27:38 -0700

    Finish up sidenote component

--- src/components/mdx/Sidenote.js
+++ src/components/mdx/Sidenote.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
+import { CgChevronDoubleRightR } from 'react-icons/cg'
 
-const SidenoteHeader = ({ children, onToggleNote }) => {
+const SidenoteHeader = ({ children, noteShow, onToggleNote }) => {
   return (
-    <div className="prose prose-lg">
+    <div className="prose prose-sm lg:prose-lg">
       {children}
-      <button className="font-bold cursor-pointer" onClick={onToggleNote}>
-        Read more &rarr;
+      <button
+        className={`relative flex align-center font-bold cursor-pointer focus:outline-none focus:ring-2 focus:ring-sea ${
+          noteShow ? 'hidden' : ''
+        }`}
+        onClick={onToggleNote}
+      >
+        Read more
+        <CgChevronDoubleRightR
+          className={`transform block relative w-8 h-8 fill-current text-steel ml-2 transition-all ${
+            noteShow ? 'rotate-90' : ''
+          }`}
+        />
       </button>
     </div>
   )
 }
 
 const SidenoteBody = ({ children, noteShow }) => {
-  return <div className={`prose prose-lg ${noteShow ? 'block' : 'hidden'}`}>{children}</div>
+  return (
+    <div className={`prose prose-sm lg:prose-lg ${noteShow ? 'block' : 'hidden'}`}>{children}</div>
+  )
 }
 
 const Sidenote = ({ children }) => {
@@ -23,10 +36,32 @@ const Sidenote = ({ children }) => {
     })
   }
 
+  // Split the `children` array at the `hr` element, which I use to separate the
+  // content that shows by default versus the content that's hidden in the
+  // collapsible portion. Those are then passed to the appropriate components.
+  //
+  // TODO: Does this work when there's no `hr`?
+  const idx = children.map((el) => el.props.mdxType).indexOf('hr')
+  const Header = children.slice(0, idx)
+  const Body = children.slice(idx + 1)
+
+  // let Header, Body
+
+  // if ( children.length > 1 ) {
+  //   const idx = children.map((el) => el.props.mdxType).indexOf('hr')
+  //   Header = children.slice(0, idx)
+  //   Body = children.slice(idx + 1)
+  // } else {
+  //   Header = []
+  //   Body = children
+  // }
+
   return (
     <aside className="!w-full bg-sea bg-opacity-10 p-6 rounded">
-      <SidenoteHeader onToggleNote={onToggleNote}>{children[0]}</SidenoteHeader>
-      <SidenoteBody noteShow={noteShow}>{children[1]}</SidenoteBody>
+      <SidenoteHeader noteShow={noteShow} onToggleNote={onToggleNote}>
+        {Header}
+      </SidenoteHeader>
+      <SidenoteBody noteShow={noteShow}>{Body}</SidenoteBody>
     </aside>
   )
 }
