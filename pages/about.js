2516b574ef6cf9bf023b7da32fcf6e1d2976375a
Joel Hans <joel.g.hans@gmail.com>
2021-08-12 16:48:49 -0700

    Working mdx-bundler

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,18 @@
 import Image from 'next/image'
-import hydrate from 'next-mdx-remote/hydrate'
 import { getSingleContent } from '@/lib/mdx'
 import { PageSeo } from '@components/SEO'
 import { BASE_CONTENT_PATH } from '@config/constants'
 import PageTitle from '@components/PageTitle'
 import siteMetadata from '@data/siteMetadata'
-import MDXComponents from '@components/MDXComponents'
+import { MDXLayoutRenderer } from '@components/MDXComponents'
 
 export async function getStaticProps() {
   const content = await getSingleContent(BASE_CONTENT_PATH, 'about')
-
-  return {
-    props: {
-      mdxSource: content.mdxSource,
-      frontMatter: content.frontMatter,
-    },
-  }
+  return { props: { content } }
 }
 
-export default function About({ mdxSource, frontMatter }) {
-  const content = hydrate(mdxSource, {
-    components: MDXComponents,
-  })
+export default function About({ content }) {
+  const { mdxSource, frontMatter } = content
 
   return (
     <>
@@ -49,7 +40,9 @@ export default function About({ mdxSource, frontMatter }) {
           />
         </div>
       </header>
-      <div className="prose prose-md lg:prose-lg xl:prose-xl dark:prose-dark mb-24">{content}</div>
+      <div className="prose prose-md lg:prose-lg xl:prose-xl dark:prose-dark mb-24">
+        <MDXLayoutRenderer mdxSource={mdxSource} frontMatter={frontMatter} />
+      </div>
     </>
   )
 }
