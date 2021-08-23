e11905ae9044bc64680528b0deddfcb04e85895c
Joel Hans <joel.g.hans@gmail.com>
2021-08-23 10:24:39 -0700

    Re-add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/media-has-caption': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
