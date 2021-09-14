665273bd383a140b59b90514817683524e104d2c
Joel Hans <joel.g.hans@gmail.com>
2021-09-14 16:22:06 -0700

    Fun with fonts

--- src/components/mdx/StoryQuote.js
+++ src/components/mdx/StoryQuote.js
@@ -1,9 +1,9 @@
 const StoryQuote = ({ children, story }) => {
   return (
-    <div className="flex items-center w-full bg-sea bg-opacity-5 p-6 rounded">
+    <div className="flex items-center w-full bg-sea bg-opacity-5 p-12 rounded">
       <div className="!m-0">
-        <p className="text-2xl leading-relaxed m-0 italic">{children}</p>
-        <p>&mdash; &ldquo;{story}&rdquo;</p>
+        <p className="text-3xl font-serif leading-relaxed !m-0 italic">{children}</p>
+        <p className="!mb-0">&mdash; &ldquo;{story}&rdquo;</p>
       </div>
     </div>
   )
