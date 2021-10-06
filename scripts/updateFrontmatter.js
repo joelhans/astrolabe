68a7c37fb585fdb03fb4bdcc53681f1ac2d3c5fd
Joel Hans <joel.g.hans@gmail.com>
2021-10-06 12:14:01 -0700

    Testing again

--- scripts/updateFrontmatter.js
+++ scripts/updateFrontmatter.js
@@ -2,19 +2,20 @@
 import { promises as fsp } from 'fs'
 import matter from 'gray-matter'
 const updateFrontmatter = async () => {
-  const [, , ...mdFilePaths] = process.argv;
+  const [, , ...mdFilePaths] = process.argv
   mdFilePaths.forEach(async (path) => {
-    const file = matter.read(path);
-    const { data: currentFrontmatter } = file;
+    const file = matter.read(path)
+    const { data: currentFrontmatter } = file
     if (currentFrontmatter.published === true) {
       const updatedFrontmatter = {
         ...currentFrontmatter,
         updatedOn: new Date().toISOString(),
-      };
-      file.data = updatedFrontmatter;
-      const updatedFileContent = matter.stringify(file);
-      fsp.writeFile(path, updatedFileContent);
+      }
+      console.log(updatedFrontmatter)
+      file.data = updatedFrontmatter
+      const updatedFileContent = matter.stringify(file)
+      fsp.writeFile(path, updatedFileContent)
     }
-  });
-};
-updateFrontmatter();
+  })
+}
+updateFrontmatter()
