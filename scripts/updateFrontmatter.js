35df815ed6c30c302248fb28bf5ed75588330160
Joel Hans <joel.g.hans@gmail.com>
2021-10-06 12:09:37 -0700

    Testing update function

---
+++ scripts/updateFrontmatter.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { promises as fsp } from 'fs'
+import matter from 'gray-matter'
+const updateFrontmatter = async () => {
+  const [, , ...mdFilePaths] = process.argv;
+  mdFilePaths.forEach(async (path) => {
+    const file = matter.read(path);
+    const { data: currentFrontmatter } = file;
+    if (currentFrontmatter.published === true) {
+      const updatedFrontmatter = {
+        ...currentFrontmatter,
+        updatedOn: new Date().toISOString(),
+      };
+      file.data = updatedFrontmatter;
+      const updatedFileContent = matter.stringify(file);
+      fsp.writeFile(path, updatedFileContent);
+    }
+  });
+};
+updateFrontmatter();
