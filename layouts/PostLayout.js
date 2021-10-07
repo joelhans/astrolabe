ecebad2dd1d3561bf63a11922f9bad64ba016339
Joel Hans <joel.g.hans@gmail.com>
2021-10-07 08:48:58 -0700

    Init new article

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -39,38 +39,40 @@ export default function PostLayout({ children, frontMatter }) {
             <div className="prose prose-md lg:prose-lg dark:prose-dark mx-auto">
               <MDXLayoutRenderer mdxSource={children} frontMatter={frontMatter} />
               <footer className="mt-24">
-                <div className="p-8 bg-steel bg-opacity-5 rounded-sm mb-12">
-                  <p className="font-bold !mt-0">Linked references</p>
-                  <div className="grid grid-flow-col grid-cols-2 gap-4 mb-8">
-                    {LinkRefs.length ? (
-                      LinkRefs.map((link) => {
-                        const { src, title, text } = link
-                        return (
-                          <CustomLink
-                            key={src}
-                            href={src}
-                            className="p-4 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-500 rounded-sm"
-                          >
-                            <span className="block text-base font-medium mb-2">{title}</span>
-                            <span className="block text-sm text-gray-500 dark:text-gray-300 font-normal">
-                              {text}
-                            </span>
-                          </CustomLink>
-                        )
-                      })
-                    ) : (
-                      <p className="text-sm text-gray-500 !-mt-4 !m-0">
-                        No linked references found.
-                      </p>
-                    )}
+                {tags.indexOf('To Mabel, To Ida') === -1 && (
+                  <div className="p-8 bg-steel bg-opacity-5 rounded-sm mb-12">
+                    <p className="font-bold !mt-0">Linked references</p>
+                    <div className="grid grid-flow-col grid-cols-2 gap-4 mb-8">
+                      {LinkRefs.length ? (
+                        LinkRefs.map((link) => {
+                          const { src, title, text } = link
+                          return (
+                            <CustomLink
+                              key={src}
+                              href={src}
+                              className="p-4 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-500 rounded-sm"
+                            >
+                              <span className="block text-base font-medium mb-2">{title}</span>
+                              <span className="block text-sm text-gray-500 dark:text-gray-300 font-normal">
+                                {text}
+                              </span>
+                            </CustomLink>
+                          )
+                        })
+                      ) : (
+                        <p className="text-sm text-gray-500 !-mt-4 !m-0">
+                          No linked references found.
+                        </p>
+                      )}
+                    </div>
+                    <p className="text-base font-bold text-gray-500 dark:text-gray-400 !my-0">
+                      Last updated:{' '}
+                      <time dateTime={date}>
+                        {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                      </time>
+                    </p>
                   </div>
-                  <p className="text-base font-bold text-gray-500 dark:text-gray-400 !my-0">
-                    Last updated:{' '}
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </p>
-                </div>
+                )}
                 <CustomLink
                   href="/articles"
                   className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
