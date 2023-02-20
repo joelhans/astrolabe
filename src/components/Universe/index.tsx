89e7cc487529557a225bc178fa813354cd3877f5
Kat <kattow88@gmail.com>
2023-02-20 01:14:28 -0600

    conditionally render sidebar
    TODO: focus management

--- src/components/Universe/index.tsx
+++ src/components/Universe/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect, FC } from 'react'
+import React, { useState, useRef, useEffect, FC, useLayoutEffect } from 'react'
 import moment from 'moment'
-import { IoTelescopeOutline } from 'react-icons/io5'
+import { IoClose, IoTelescopeOutline } from 'react-icons/io5'
 import Image from 'next/image'
 import StarscapeData from '@data/stars.json'
 import { Post, StarLink } from '@/types/content'
@@ -12,8 +12,10 @@ import {
   createStars,
   createUniverse,
   createStarscape,
+  xScale,
+  yScale,
 } from './createUniverse'
-import { resetHighlight } from './highlight'
+import { highlight, resetHighlight } from './highlight'
 
 const getVisitedPosts = (posts: Post[]): Post[] => {
   // Set the `visitedStars` cookie to an empty array if there is no localStorage
@@ -33,11 +35,22 @@ const getVisitedPosts = (posts: Post[]): Post[] => {
 }
 
 const Universe: FC<{ posts: Post[] }> = ({ posts: incomingPosts }) => {
-  const [tooltipData, setTooltipData] = useState<any | null>(null)
+  const [tooltipData, setTooltipData] = useState<Post | null>(null)
+  const [tooltipStyle, setTooltipStyle] = useState('-right-full')
   const closeTooltip = () => {
-    resetHighlight()
-    setTooltipData(null)
+    setTooltipStyle('-right-full')
+    setTimeout(() => {
+      resetHighlight()
+      setTooltipData(null)
+    }, 150)
   }
+  useEffect(() => {
+    if (tooltipData) {
+      setTimeout(() => {
+        setTooltipStyle('right-0')
+      }, 150)
+    }
+  }, [tooltipData])
 
   useEffect(() => {
     document.body.style.overflow = 'hidden'
@@ -52,8 +65,10 @@ const Universe: FC<{ posts: Post[] }> = ({ posts: incomingPosts }) => {
 
   const asterismRef = useRef<SVGSVGElement>(null)
   const tooltipRef = useRef<HTMLDivElement>(null)
+  const tooltipLinkRef = useRef<HTMLAnchorElement>(null)
+
   useEffect(() => {
-    if (asterismRef.current && tooltipRef.current) {
+    if (asterismRef.current) {
       const posts = getVisitedPosts(incomingPosts)
       const universe = createUniverse(asterismRef.current)
       const asterisms = createAsterisms(posts)
@@ -62,7 +77,7 @@ const Universe: FC<{ posts: Post[] }> = ({ posts: incomingPosts }) => {
       createLines(stars, links)
       createNames(stars, asterisms)
     }
-  }, [incomingPosts, asterismRef, tooltipRef])
+  }, [asterismRef, incomingPosts])
 
   return (
     <>
@@ -84,49 +99,53 @@ const Universe: FC<{ posts: Post[] }> = ({ posts: incomingPosts }) => {
               </radialGradient>
             </defs>
           </svg>
-          <div
-            ref={tooltipRef}
-            className={`tooltipScatter absolute bottom-0 md:top-0 ${
-              tooltipData ? `right-0` : `-right-full`
-            } block w-full md:h-screen md:w-4/12 lg:3/12 flex flex-col justify-center text-xl !text-gray-900 p-8 lg:p-12 bg-gray-100 rounded-sm transition-all ease-in-out`}
-          >
-            <div>
-              <p className="text-3xl lg:text-3xl 2xl:text-4xl font-serif font-bold mb-6">
-                {tooltipData?.title}
-              </p>
-              {tooltipData?.author && (
-                <p className="text-base 2xl:text-lg font-sans font-medium mb-6">
-                  Materialized by <span className="text-pink font-bold">{tooltipData?.author}</span>{' '}
-                  on {moment(tooltipData?.publishedOn).format('dddd, MMMM Do YYYY')}.
-                </p>
-              )}
-              {tooltipData?.summary && (
-                <p className="prose md:prose-lg 2xl:prose-2xl italic">{tooltipData?.summary}</p>
-              )}
-              {tooltipData?.artworkUrl && (
-                <div className="blur-sm block relative max-h-64 overflow-hidden mt-6">
-                  <Image src={tooltipData?.artworkUrl} width={400} height={200} alt="TODO" />
-                </div>
-              )}
-              <button className="font-sans text-base lg:text-lg font-medium text-white mt-6 bg-gradient-to-tr from-[#0d1c48] to-[#0f062d] rounded-sm transition-all ease-in-out hover:brightness-150">
-                <a href={tooltipData?.slug} className="block px-4 py-3">
+          {tooltipData && (
+            <div
+              ref={tooltipRef}
+              className={`absolute bottom-0 md:top-0 ${tooltipStyle} block w-full md:h-screen md:w-4/12 lg:3/12 flex flex-col justify-center text-xl !text-gray-900 p-8 lg:p-12 bg-gray-100 rounded-sm transition-all ease-in-out`}
+            >
+              <button
+                className="absolute top-6 right-6 font-sans font-medium text-3xl"
+                onClick={closeTooltip}
+              >
+                <IoClose />
+              </button>
+              <div>
+                <h2 className="text-3xl lg:text-3xl 2xl:text-4xl font-serif font-bold mb-6">
+                  {tooltipData?.title}
+                </h2>
+                {tooltipData?.author && (
+                  <p className="text-base 2xl:text-lg font-sans font-medium mb-6">
+                    Materialized by{' '}
+                    <span className="text-pink font-bold">{tooltipData?.author}</span> on{' '}
+                    {moment(tooltipData?.publishedOn).format('dddd, MMMM Do YYYY')}.
+                  </p>
+                )}
+                {tooltipData?.summary && (
+                  <p className="prose md:prose-lg 2xl:prose-2xl italic">{tooltipData?.summary}</p>
+                )}
+                {tooltipData?.artworkUrl && (
+                  <div className="blur-sm block relative max-h-64 overflow-hidden mt-6">
+                    <Image src={tooltipData?.artworkUrl} width={400} height={200} alt="TODO" />
+                  </div>
+                )}
+                <a
+                  ref={tooltipLinkRef}
+                  href={tooltipData?.slug}
+                  aria-label={tooltipData?.title}
+                  className="inline-block w-auto px-4 py-3 font-sans text-base lg:text-lg font-medium text-white mt-6 bg-gradient-to-tr from-[#0d1c48] to-[#0f062d] rounded-sm transition-all ease-in-out hover:brightness-150"
+                >
                   Take a look{` `}
                   <IoTelescopeOutline className="inline w-4 h-4 ml-1 text-gray-100" />
                 </a>
-              </button>
-              {tooltipData?.visited && (
-                <p className="text-sm lg:text-base text-pink font-sans font-medium mt-8">
-                  You've visited this star before.
-                </p>
-              )}
+                {tooltipData?.visited && (
+                  <p className="text-sm lg:text-base text-pink font-sans font-medium mt-8">
+                    You've visited this star before.
+                  </p>
+                )}
+              </div>
             </div>
-            <button
-              className="absolute top-6 right-6 font-sans font-medium text-3xl"
-              onClick={closeTooltip}
-            >
-              x
-            </button>
-          </div>
+          )}
         </div>
       </div>
     </>
