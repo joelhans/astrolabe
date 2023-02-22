da42b2e1fabc81d5c7a511d4b8009fe9196f1525
Joel Hans <j@joelhans.com>
2023-02-22 09:40:11 -0700

    Add clarity to submission windows and remove the open/close box at the top

--- src/components/mdx/Submissions.js
+++ src/components/mdx/Submissions.js
@@ -1,5 +1,4 @@
 import moment from 'moment'
-import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
 
 const FreePaid = ({ paid, date }) => {
   const closeDate = Date.parse(date)
@@ -23,70 +22,95 @@ const FreePaid = ({ paid, date }) => {
   )
 }
 
-const Guidelines = ({ paid }) => {
+const Guidelines = ({ paid, dateFreeOpen, dateFreeClose }) => {
   return (
     <>
       {paid ? (
         <>
+          <p>
+            We are currently open to <strong>paid</strong> submissions. If you&rsquo;d prefer to not
+            pay a fee, our next free submission window is open between{' '}
+            {moment(dateFreeOpen).format('dddd, MMMM Do')} and{' '}
+            {moment(dateFreeClose).format('dddd, MMMM Do, YYYY')}.
+          </p>
           <p>To send a paid submission:</p>
           <ol>
             <li>
-              <span>
+              <span className="block mb-2">
                 Send your submission to{' '}
                 <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a> with the subject
                 line:
               </span>
-              <br />
-              <strong>Paid submission: Your Name, &ldquo;Title&rdquo;</strong>
-              <br />
+              <strong className="block mb-2">
+                Paid submission: Your Name, &ldquo;Title&rdquo;
+              </strong>
+              <span className="block mb-2">
+                Include a short cover letter, bio, and your work as an attachment(s).
+              </span>
               <span>
-                Include a short cover letter, bio, and your work as an attachment(s). No need to
-                send us your physical address or phone number.
+                We welcome pen names, author names, or pseudonyms. No need to include your legal
+                name, physical address, phone number, or other personal information with your
+                submission&mdash;the name you publish under and your email address is enough!
               </span>
             </li>
             <li>
-              Click the <strong>PayPal</strong> button below to pay the $5 fee.
+              <span className="block mb-2">
+                Click the <strong>PayPal</strong> button below to pay the $5 fee.
+              </span>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="7PZ9LDC4Q7FB4" />
+                <input
+                  className="px-4 py-3 border rounded hover:bg-gray-200 transition-all"
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
+                  border="0"
+                  name="submit"
+                  alt="PayPal - The safer, easier way to pay online!"
+                />
+              </form>
             </li>
           </ol>
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-            <input type="hidden" name="cmd" value="_s-xclick" />
-            <input type="hidden" name="hosted_button_id" value="7PZ9LDC4Q7FB4" />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
-              border="0"
-              name="submit"
-              alt="PayPal - The safer, easier way to pay online!"
-            />
-          </form>
+
           <p>
             We&rsquo;ll send a confirmation to let you know we received your submission and fee.
           </p>
           <p>
             We&rsquo;ll get back to you within <strong>one month</strong>&mdash;if we take longer,
-            feel free to <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a>. We're
-            strong believers that Wednesday is the least painful day of the week to send (or
-            receive) a rejection.
+            feel free to <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a>.
+            We&rsquo;re strong believers that Wednesday is the least painful day of the week to send
+            (or receive) a rejection.
           </p>
         </>
       ) : (
         <>
           <p>
-            Send submissions to <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a>.
+            We are currently <strong>open</strong> for free submissions until{' '}
+            {moment(dateFreeClose).format('dddd, MMMM Do, YYYY')}!
           </p>
-          <p>
-            Please format the subject line like so:{' '}
-            <strong>Submission: Your Name, &ldquo;Title&rdquo;</strong>
-          </p>
-          <p>
-            Include a short cover letter, bio, and your work as an attachment(s). No need to send us
-            your physical address or phone number.
-          </p>
+          <ol>
+            <li>
+              <span className="block mb-2">
+                Send your submission to{' '}
+                <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a> with the subject
+                line:
+              </span>
+              <strong className="block mb-2">Submission: Name, &ldquo;Title&rdquo;</strong>
+              <span className="block mb-2">
+                Include a short cover letter, bio, and your work as an attachment(s).
+              </span>
+              <span>
+                We welcome pen names, author names, or pseudonyms. No need to include your legal
+                name, physical address, phone number, or other personal information with your
+                submission&mdash;the name you publish under and your email address is enough!
+              </span>
+            </li>
+          </ol>
           <p>
             We&rsquo;ll get back to you within <strong>one month</strong>&mdash;if we take longer,
-            feel free to <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a>. We're
-            strong believers that Wednesday is the least painful day of the week to send (or
-            receive) a rejection.
+            feel free to <a href="mailto:editors@astrolabe.ooo">editors@astrolabe.ooo</a>.
+            We&rsquo;re strong believers that Wednesday is the least painful day of the week to send
+            (or receive) a rejection.
           </p>
         </>
       )}
