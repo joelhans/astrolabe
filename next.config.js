53445babaf1b176ebcfb6152489f448eec76a52c
Joel Hans <j@joelhans.com>
2023-12-15 14:48:40 -0700

    Clean up deps

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
   eslint: {
     dirs: ['app', 'pages', 'components', 'lib', 'layouts', 'scripts'],
   },
-  // experimental: {
-  //   appDir: true,
-  // },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -20,32 +17,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// module.exports = {
-//   reactStrictMode: false,
-//   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-//   eslint: {
-//     dirs: ['app', 'pages', 'components', 'lib', 'layouts', 'scripts'],
-//   },
-//   experimental: {
-//     appDir: true,
-//   },
-//   webpack: (config, { dev, isServer }) => {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack'],
-//     })
-
-//     // if (!dev && !isServer) {
-//     //   // Replace React with Preact only in client production build
-//     //   Object.assign(config.resolve.alias, {
-//     //     'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-//     //     react: 'preact/compat',
-//     //     'react-dom/test-utils': 'preact/test-utils',
-//     //     'react-dom': 'preact/compat',
-//     //   })
-//     // }
-
-//     return config
-//   },
-// }
